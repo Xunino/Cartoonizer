3deaab7677fed636c72667c1cd384576c6525e97
xunino <ndlinh.ai@gmail.com>
2022-01-11 21:34:29 +0700

    first commit

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ class Trainer:
                  image_shape=256,
                  learning_rate=2e-4,
                  channels=32,
-                 batch_size=32):
+                 batch_size=32,
+                 use_parallel=True):
         self.photo_face_path = photo_face_path
         self.cartoon_face_path = cartoon_face_path
 
@@ -35,6 +36,7 @@ class Trainer:
         self.image_shape = image_shape
         self.batch_size = batch_size
         self.channels = channels
+        self.use_parallel = use_parallel
 
         self.loss_min = 2.0
 
@@ -67,25 +69,28 @@ class Trainer:
         self.ckpt_disc_manager = tf.train.CheckpointManager(ckpt_disc, self.saved_disc_weights, max_to_keep=2)
 
     def train_step(self):
-        # Photo face and cartoon face
-        input_photo_face, len_photo_face = DataLoader(self.photo_face_path, image_shape=self.image_shape,
-                                                      batch_size=self.batch_size).run()
-        input_cartoon_face, len_cartoon_face = DataLoader(self.cartoon_face_path, image_shape=self.image_shape,
-                                                          batch_size=self.batch_size).run()
-
-        # Photo scenery and cartoon scenery
-        input_photo_scenery, len_photo_scenery = DataLoader(self.photo_scenery_path, image_shape=self.image_shape,
-                                                            batch_size=self.batch_size).run()
-        input_cartoon_scenery, len_cartoon_scenery = DataLoader(self.cartoon_scenery_path, image_shape=self.image_shape,
-                                                                batch_size=self.batch_size).run()
-
-        min_len = min(len_photo_face, len_cartoon_face, len_photo_scenery, len_cartoon_scenery)
 
         # Start training
         for epoch in range(self.epochs + 1):
             g_loss = 0
             d_loss = 0
-            pbar = tqdm(range(min_len // self.batch_size + 1))
+
+            # Photo face and cartoon face
+            input_photo_face, len_photo_face = DataLoader(self.photo_face_path, image_shape=self.image_shape,
+                                                          batch_size=self.batch_size).run()
+            input_cartoon_face, len_cartoon_face = DataLoader(self.cartoon_face_path, image_shape=self.image_shape,
+                                                              batch_size=self.batch_size).run()
+
+            # Photo scenery and cartoon scenery
+            input_photo_scenery, len_photo_scenery = DataLoader(self.photo_scenery_path, image_shape=self.image_shape,
+                                                                batch_size=self.batch_size).run()
+            input_cartoon_scenery, len_cartoon_scenery = DataLoader(self.cartoon_scenery_path,
+                                                                    image_shape=self.image_shape,
+                                                                    batch_size=self.batch_size).run()
+
+            min_len = min(len_photo_face, len_cartoon_face, len_photo_scenery,
+                          len_cartoon_scenery) // self.batch_size + 1
+            pbar = tqdm(range(min_len))
             for iterator in pbar:
                 if iterator % 5 == 0:
                     input_photo = input_photo_face
@@ -113,7 +118,7 @@ class Trainer:
 
                     vgg_photo = self.vgg(input_photo)
                     vgg_output = self.vgg(output)
-                    superpixel_out = simple_superpixel(output)
+                    superpixel_out = simple_superpixel(output, use_parallel=self.use_parallel)
                     vgg_superpixel = self.vgg(superpixel_out)
 
                     photo_loss = content_loss(vgg_photo, vgg_output)
@@ -122,8 +127,8 @@ class Trainer:
                     recon_loss = photo_loss + superpixel_loss
                     tv_loss = total_variation_loss(output)
 
-                    g_loss += 1e4 * tv_loss + 1e-1 * g_loss_blur + g_loss_gray + 2e2 * recon_loss
-                    d_loss += d_loss_blur + d_loss_gray
+                    g_loss += (1e4 * tv_loss + 1e-1 * g_loss_blur + g_loss_gray + 2e2 * recon_loss) / self.batch_size
+                    d_loss += (d_loss_blur + d_loss_gray) / self.batch_size
 
                     # Generator
                     gradients_of_generator = gen_tape.gradient(g_loss, self.generator.trainable_variables)
@@ -133,10 +138,9 @@ class Trainer:
                     gradients_of_discriminator = disc_tape.gradient(d_loss, self.disc_sn.trainable_variables)
                     self.d_optimizer.apply_gradients(zip(gradients_of_discriminator, self.disc_sn.trainable_variables))
 
-                    if iterator % 100 == 0:
-                        pbar.set_description("Epoch {} || g_loss_total: {} || d_loss_total: {}".format(epoch,
-                                                                                                       g_loss,
-                                                                                                       d_loss))
+                    pbar.set_description("Epoch {} || g_loss_total: {} || d_loss_total: {}".format(epoch,
+                                                                                                   g_loss,
+                                                                                                   d_loss))
 
                     if d_loss < self.loss_min:
                         results = self.generator(input_photo)
@@ -145,8 +149,8 @@ class Trainer:
                         self.ckpt_disc_manager.save()
                         self.loss_min = d_loss
 
-            g_loss_total = g_loss / self.batch_size
-            d_loss_total = d_loss / self.batch_size
+            g_loss_total = g_loss / min_len
+            d_loss_total = d_loss / min_len
             print("Epoch {} || g_loss_total: {} || d_loss_total: {}".format(epoch,
                                                                             g_loss_total,
                                                                             d_loss_total))
@@ -159,15 +163,21 @@ if __name__ == '__main__':
         cartoon_faces = "dataset/cartoon_faces"
         real_scenery = "dataset/scenery"
         cartoon_scenery = "dataset/cartoon_scenery"
+        train = Trainer(real_face, cartoon_faces,
+                        real_scenery, cartoon_scenery,
+                        image_shape=128, epochs=5,
+                        batch_size=6, channels=8, use_parallel=True)
     else:
         real_face = "/content/drive/MyDrive/dataset/faces"
         cartoon_faces = "/content/drive/MyDrive/dataset/cartoon_faces"
         real_scenery = "/content/drive/MyDrive/dataset/scenery"
         cartoon_scenery = "/content/drive/MyDrive/dataset/cartoon_scenery"
-
-    train = Trainer(real_face, cartoon_faces,
-                    real_scenery, cartoon_scenery,
-                    image_shape=256, epochs=5,
-                    batch_size=16, channels=8)
+        train = Trainer(real_face, cartoon_faces,
+                        real_scenery, cartoon_scenery,
+                        image_shape=128, epochs=5,
+                        batch_size=6, channels=8, use_parallel=False)
 
     train.train_step()
+    """
+    Epoch 0 || g_loss_total: 2.736996650695801 || d_loss_total: 0.9677947759628296
+    """
